de85e35b676c142a652edcb076a1d2084dbc0df1
Boris Bronovitsky <bronovitsky@gmail.com>
2021-07-10 11:46:28 +0300

    new .gitignore && env_vars

---
+++ set_env_vars.cmd
@@ -0,0 +1,9 @@
+@echo off
+rem set JAVA8_HOME=
+rem set JAVA11_HOME=
+set JAVA14_HOME=C:\tools\jdk\jdk-14.0.2
+set JAVA16_HOME=C:\tools\jdk\oracle_jdk.16.0.1
+set JAVA_HOME=%JAVA14_HOME%
+set NODE14_HOME=C:\tools\nodejs\node-v14.17.1-win-x64
+set NODE_HOME=%NODE14_HOME%
+set Path=%Path%;%JAVA_HOME%\bin;%NODE_HOME%
